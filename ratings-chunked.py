f6b2bf69a4d92a33dad9aab8d669a92bfcedd1a3
AJ Boyd <alanjaredboyd3@yahoo.com>
2024-12-16 21:43:58 -0500

    after running main_ratings.py, run this for the CHUNKED analysis

---
+++ ratings-chunked.py
@@ -0,0 +1,58 @@
+"""
+auth: AJ Boyd
+desc: once you've got a json file, we can start our emotional analysis. 
+this script iterates through each movie dialogue in each rating range then records the emotion evoked from that dialogue.
+this data is recorded and stored in a different json file. the data can be visuallized with a graph.
+WARNING: this stuff takes a loooooooong time.
+"""
+
+from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForCausalLM
+import json
+
+# download the model fine-tuned for emotional analysis
+tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-emotion")
+model = AutoModelWithLMHead.from_pretrained("mrm8488/t5-base-finetuned-emotion")
+
+# helper function predicts the sentiment from a given string
+# returns one of six emotions: joy, anger, sadness, surprise, anger, or love
+def get_emotion(text):
+  input_ids = tokenizer.encode(text + '</s>', return_tensors='pt')
+  output = model.generate(input_ids=input_ids, max_length=2)
+  dec = [tokenizer.decode(ids) for ids in output]
+  label = dec[0]
+  return label.split()[-1]
+
+with open("ratings_dict.txt", "r", encoding="utf-8") as f:
+    rating_dict = json.load(f)
+print('dictionary loaded in')
+
+emotions_by_rating = []
+i = 0
+
+for rating in rating_dict:
+    # build a dictionary that holds the counts for every emotion per genre    
+    emotion_dict = {'rating': rating, 'joy': 0, 'sadness': 0, 'anger': 0, 'surprise': 0, 'fear': 0, 'love': 0}
+
+    # get every text interaction for the genre
+    for dialog_dict in rating_dict[rating]:
+        try:
+            text = "\n".join(dialog_dict['utterance']['text'])
+            emotion = get_emotion(text) # get the analyzed emotion from that interaction
+            emotion_dict[emotion] += 1 # increase the count of that emotion by 1   
+            i += 1
+            print(f"{i}--{text}")
+        except Exception as e:
+            print(f"error getting sentiment \ntext: {text}\nerror: {e}") 
+    
+    
+    emotions_by_rating.append(emotion_dict)
+
+
+# write to file
+try:
+    with open("genre-unchunked.txt", "w", encoding="utf-8") as f:
+        json.dumps(emotions_by_rating, f, ensure_ascii=False, indent=1)
+except Exception as e:
+    print(f"error: {e}")
+   
+        
